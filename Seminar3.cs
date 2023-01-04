af1762efaf1836eb5c29cb79f238ab7cfa839677
Semion Volkov <semen_volkov@mail.ru>
2023-01-04 16:00:07 +0500

    Task2 added

--- Seminar3.cs
+++ Seminar3.cs
@@ -33,4 +33,42 @@ void Palindrom()
 
 }
 
-Palindrom();
+//Palindrom();
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double Distance (double[] A, double[] B)
+{
+    double dist = 0;
+
+    for (int i = 0; i < A.Length; i++)
+    {
+        dist = dist + Math.Pow(B[i] - A[i], 2);
+    }
+
+    dist = Math.Round(Math.Sqrt(dist), 2);
+
+    Console.WriteLine(dist);
+
+    return dist;
+}
+
+double[] GetCoordinates()
+{
+    string[] coordinateNames = {"x", "y", "z"};
+    double[] point = new double[3];
+    Console.WriteLine("Введите координаты следующей точки");
+    for (int j = 0; j < point.Length; j++)
+    {
+        Console.WriteLine($"Введите координату {coordinateNames[j]}: ");
+        point[j] = double.Parse(Console.ReadLine());
+    }
+    
+    return point;
+}
+
+Distance(GetCoordinates(), GetCoordinates());
